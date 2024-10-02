603fed832b691a89db8d881786a446b1144a3164
Sparsh Rastogi <96334601+SparshR21@users.noreply.github.com>
2024-10-02 12:53:27 +0530

    Update Admin.jsx

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import './Admin.css'
-import Sidebar from '../../components/Sidebar/Sidebar'
-import { Routes,Route } from 'react-router-dom'
-import AddProduct from '../../components/AddProduct/AddProduct'
-import ListProduct from '../../components/ListProduct/ListProduct'
+import React from 'react';
+import './Admin.css';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import AddProduct from '../../components/AddProduct/AddProduct';
+import ListProduct from '../../components/ListProduct/ListProduct';
+import { useAuth } from '../../AuthContext';
 
 const Admin = () => {
+  const { isAuthenticated } = useAuth();
+
+  // If not authenticated, redirect to login or another route
+  if (!isAuthenticated) {
+    return <Navigate to="/" />; // Redirect to login or main page
+  }
+
   return (
     <div className='admin'>
-      <Sidebar/>
+      <Sidebar />
       <Routes>
-        <Route path='/addproduct' element={<AddProduct/>} />
-        <Route path='/listProduct' element={<ListProduct/>} />
+        <Route path='/addproduct' element={<AddProduct />} />
+        <Route path='/listProduct' element={<ListProduct />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default Admin
+export default Admin;
